fb578572780bd148ededdce72113a6f68a8ee7fb
Kirill Kouzoubov <kirill888@gmail.com>
2019-08-16 09:48:07 +1000

    Don't bother with docker load/save it's too slow

--- travis/cache.sh
+++ travis/cache.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+# This is not used currently as it was too slow to do `docker load|save`
+# But we might revisit this idea.
+#
+#  1. Load docker state from cache using `docker load` or pull image from hub
+#  2. Build image with `docker build --cache-from`
+#  3. Save docker images to cache `docker save`
+#
+# Env:
+#  CACHE_DIR
+#  CACHE_FILE
+#  IMAGE_BASE
+#  IMAGE
+#  CACHE_IMAGE
 
 load_cache () {
     if [[ -f ${CACHE_FILE} ]]; then
         docker load -i ${CACHE_FILE}
     else
-        echo "no local cache will pull ${IMAGE_BASE}:latest";
+        echo "no local cache will pull ${IMAGE_BASE}:latest"
         docker pull "${IMAGE_BASE}:latest"
         docker tag "${IMAGE_BASE}:latest" "${CACHE_IMAGE}"
     fi
@@ -16,7 +29,7 @@ save_cache () {
     mkdir -p ${CACHE_DIR}
     docker tag "${IMAGE}" "${CACHE_IMAGE}"
     docker images
-    docker save "${CACHE_IMAGE}" | gzip -2 > ${CACHE_FILE}
+    docker save "${CACHE_IMAGE}" > ${CACHE_FILE}
 }
 
 
